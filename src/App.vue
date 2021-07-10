358caaac094d34f57b9b3b6284f438da4d27a699
Matej <matejchalachan@gmail.com>
2021-07-10 23:46:15 +0200

    'init'

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <StructureView :data="data" v-on:selected="itemSelected" />
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import StructureView from "./components/StructureView.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+      StructureView
+  },
+    methods: {
+        itemSelected: function () {
+
+        }
+    },
+    computed: {
+      data: function () {
+          return {
+              first: 'level',
+              works: true,
+              number: 100,
+              missing: null,
+              undefined: undefined,
+              list: [
+                  'fun',
+                  {
+                      test: {
+                          passed: true
+                      }
+                  }
+              ],
+              object: {
+                  working: 'properly'
+              }
+          };
+      }
+    }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StructureView.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+    name: 'StructureView',
+    props: {
+    },
+    setup: () => {
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
